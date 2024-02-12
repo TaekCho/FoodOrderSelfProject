37ba14ec61fc570a024f107a7b5769623e88783e
JT Cho <jtcho123@gmail.com>
2024-02-11 22:50:00 -0500

    implement useReducer to add & remove item in cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import Button from "./UI/Button";
 import logo from "../assets/logo.jpg";
+import CartContext from "../store/CartContext";
+import { useContext } from "react";
 
 export default function Header({ itemsInCart }) {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
   return (
     <header id="main-header">
       <div id="title">
@@ -10,7 +18,7 @@ export default function Header({ itemsInCart }) {
       </div>
       <nav>
         {/* shows how many items are in the cart */}
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -8,22 +8,79 @@ const CartContext = createContext({
 
 function cartReducer(state, action) {
   if (action.type === "ADD_ITEM") {
-    // ... update the state to add a meal item
-    state.items.push(action.item); // THIS IS WRONG because
-    // state should never be updated imperatively.
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const updatedItems = [...state.items];
+
+    // When findIndex cannot find the item, it will return -1.
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      // update state in an immutable way without changing
+      // the existing state in memory.
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+    // return object so as not to edit the existing state
+    return { ...state, items: updatedItems };
   }
 
   if (action.type === "REMOVE_ITEM") {
     // ... remove an item from the state
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      // splice takes an index, and then the number of items will be spliced, or removed.
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItem[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
   }
 
   return state;
 }
 
 export function CartContextProvider({ children }) {
-  useReducer(cartReducer, { items: [] });
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
-  return <CartContext.Provider>{children}</CartContext.Provider>;
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item }); // === item: item
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  const cartContext = {
+    // When `cart` in this function above changes, the one below will also change and will be
+    // distributed to related components.
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+  console.log(cartContext);
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartContext;

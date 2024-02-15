76c9f106d81d648a89e6f88d0decb1070bbf17ac
JT Cho <jtcho123@gmail.com>
2024-02-14 21:27:40 -0500

    add checkout modal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,17 +1,25 @@
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-export default function Modal({ children, className = "", open }) {
+export default function Modal({ children, className = "", open, onClose }) {
   const dialog = useRef();
 
   useEffect(() => {
+    // store the dialog in a temporary const because
+    // the clean up function will be executed later.
+    // In this example, the value is not changing,
+    // but it could be changed when clean up function does.
+    const modal = dialog.current;
+
     if (open) {
-      dialog.current.showModal();
+      modal.showModal();
     }
+
+    return () => modal.close();
   }, [open]);
 
   return createPortal(
-    <dialog ref={dialog} className={`modal ${className}`}>
+    <dialog ref={dialog} className={`modal ${className}`} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById("modal")
